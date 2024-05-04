7da25b32077dcef2e1cef505dd3d0e28614ba76c
Batug Kocak <batugkocak@gmail.com>
2024-05-04 14:32:28 +0300

    feat: Done Login and Register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,36 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError } = require("../errors");
-const bcrypt = require("bcryptjs");
-
-const login = async (req, res, next) => {
-  res.send("Login User");
-};
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res, next) => {
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res
+    .status(StatusCodes.CREATED)
+    .json({ user: { name: user.getName() }, token });
+};
+
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+  const user = await User.findOne({ email });
+  //compare password
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+  const isMatch = await user.comparePassword(password);
+
+  if (isMatch) {
+    const token = user.createJWT();
+    return res
+      .status(StatusCodes.OK)
+      .json({ user: { name: user.name }, token });
+  }
+
+  throw new BadRequestError("Password is wrong");
 };
 
 module.exports = {
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -29,4 +29,32 @@ userSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+userSchema.methods.getName = function () {
+  return this.name;
+};
+
+userSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
+};
+
+// Bcrypt hides the salt, within the password while hashing
+// For the same password, password hashes is:
+// 1- $2a$10$45RdRWov13Rt1GK0DsZLrOADiS/bLqNCF009sBQeA8WCR4/9OFxOG
+// 2- $2a$10$mDm8hDTefVvFseoTJnSLSuDvgmw2vWiSzqwvMrDh8JCIDqRa/r2Au
+// and the saltes is:
+// $2a$10$45RdRWov13Rt1GK0DsZLrO
+// $2a$10$mDm8hDTefVvFseoTJnSLSu
+// So salt is actually is IN the password, first x character is salt
+// and the other characters are : hash(salt+password)
+userSchema.methods.comparePassword = async function (canditatePassword) {
+  const isMatch = await bcrypt.compare(canditatePassword, this.password);
+  return isMatch;
+};
 module.exports = mongoose.model("User", userSchema);
